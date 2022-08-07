419cda9577988f0d9a76281845f01672594d4029
Zetta Eric Julianto <algonacci1@gmail.com>
2022-08-07 19:04:50 +0700

    display all messages

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
+import datetime
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+from dotenv import dotenv_values
+
 
 app = Flask(__name__)
 
-@app.route('/')
+config = dotenv_values(".env")
+client = MongoClient(config["MONGODB_URI"])
+app.db = client.secreto
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        message = request.form['message']
+        formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+        app.db.messages.insert_one({"message": message, "date": formatted_date})
+        messages = [
+            (message["message"],
+             message["date"],
+             datetime.datetime.today().strftime("%Y-%m-%d") == message["date"])
+            for message in app.db.messages.find({})
+        ]
+        return render_template('index.html', messages=messages, title="Recent Messages")
+    else:
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
